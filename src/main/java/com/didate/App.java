227320c0717a36d1719b9c52f394e38173c0e0cd
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2024-08-03 00:24:29 -0400

    You are given an array of binary int s ( containing only 0 and 1). You may choose up to one 0 and flip it to a 1. What is the length of the longest subarray achievable that contains only 1

--- src/main/java/com/didate/App.java
+++ src/main/java/com/didate/App.java
@@ -13,7 +13,9 @@ public class App
         int[] nums ={9, 1, 2, 7, 4, 2, 1, 1, 5};
         System.out.println(slidingWindows.lengthLongestSubarraySumLessThanEqualK(nums, 8));
     
-       
+        int[] nums2 ={1,1,0,1,1,0,0,1,1,1};
+        System.out.println(slidingWindows.lengthOfLongestSubarrayContaining0(nums2));
+    
 
 
     }
--- src/main/java/com/didate/SlidingWindows.java
+++ src/main/java/com/didate/SlidingWindows.java
@@ -23,7 +23,34 @@ public class SlidingWindows {
         }
         return length;
     }
+    
+    /**
+     * You are given an array of binary int s ( containing only 0 and 1).
+     * You may choose up to one "0" and flip it to a "1".
+     * What is the length of the longest subarray achievable that contains only "1"?
+     */
+    public int lengthOfLongestSubarrayContaining0(int[] nums){
 
+        //s = "110110111"
+        int left = 0;
+        int length=0;
+        int numberOf0=0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            if(nums[right]==0){
+                numberOf0++;
+            }
+            while (numberOf0 > 1) {
+                if(nums[left]==0) 
+                    numberOf0--;
+                left++;
+            }
+            length = Math.max(length, right-left+1);
+            
+        }
+        return length;
+    }
    
 
 }
