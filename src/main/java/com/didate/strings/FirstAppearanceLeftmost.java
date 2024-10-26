bcaff6fc57d5d7bb2ef051031f0a5311e361aae5
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2024-10-25 20:02:22 -0400

    You are given a string S (both uppercase and lowercase characters). You need to print the index of repeated character whose first appearance is leftmost

--- src/main/java/com/didate/strings/FirstAppearanceLeftmost.java
+++ src/main/java/com/didate/strings/FirstAppearanceLeftmost.java
@@ -2,26 +2,32 @@ package com.didate.strings;
 
 import java.util.LinkedHashMap;
 
+/**
+ * You are given a string S (both uppercase and lowercase characters). 
+ * You need to print the index of repeated character whose first appearance is leftmost
+ * S = geeks
+ * Output: 0
+ */
 public class FirstAppearanceLeftmost {
-    
-     static int repeatedCharacter(String S)
-    {
+
+    static int repeatedCharacter(String S) {
         LinkedHashMap<Character, Integer[]> repeat = new LinkedHashMap<>();
-        
-        for(int i=0; i<S.length(); i++){
+
+        for (int i = 0; i < S.length(); i++) {
             Character currentChar = S.charAt(i);
             Integer[] currentValue = repeat.get(currentChar);
-            
-            if(currentValue==null){
-                repeat.put(currentChar, new Integer[]{1, i});
-            }else{
+
+            if (currentValue == null) {
+                repeat.put(currentChar, new Integer[] { 1, i });
+            } else {
                 currentValue[0]++;
                 repeat.put(currentChar, currentValue);
             }
         }
 
-        for(Integer[] value : repeat.values()){
-            if(value[0]>1) return value[1];
+        for (Integer[] value : repeat.values()) {
+            if (value[0] > 1)
+                return value[1];
         }
         return -1;
     }
