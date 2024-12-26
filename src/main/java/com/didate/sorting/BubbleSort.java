7b501f1dbd9d5a735c8d0a50278b7e074ca6da18
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2024-12-25 23:04:37 -0500

    improving Bubble sort

--- src/main/java/com/didate/sorting/BubbleSort.java
+++ src/main/java/com/didate/sorting/BubbleSort.java
@@ -9,9 +9,25 @@ public class BubbleSort {
         for(int i =0; i<n-1; i++){
             for(int j=0; j<n-i-1; j++){
                 if(arr[j] > arr[j+1]){
-                    Swapper.swap(arr, j, j+1);
+                    SortUtils.swap(arr, j, j+1);
                 }
             }
+        }
+    }
+
+    public static void sortImproved(int[] arr){
+
+        int n = arr.length;
+
+        for(int i =0; i<n-1; i++){
+            boolean isSwapped = false;
+            for(int j=0; j<n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    SortUtils.swap(arr, j, j+1);
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped) return;
         }
     }
 }
--- src/test/java/com/didate/sorting/BubbleSortTest.java
+++ src/test/java/com/didate/sorting/BubbleSortTest.java
@@ -52,4 +52,53 @@ public class BubbleSortTest {
         BubbleSort.sort(arr);
         assertArrayEquals(expected, arr);
     }
+
+
+    @Test
+    public void testSortImproved_AlreadySortedArray() {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] expected = {1, 2, 3, 4, 5};
+        BubbleSort.sortImproved(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testSortImproved_UnsortedArray() {
+        int[] arr = {5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5};
+        BubbleSort.sortImproved(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testSortImproved_ArrayWithDuplicates() {
+        int[] arr = {4, 2, 2, 8, 5};
+        int[] expected = {2, 2, 4, 5, 8};
+        BubbleSort.sortImproved(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testSortImproved_SingleElementArray() {
+        int[] arr = {1};
+        int[] expected = {1};
+        BubbleSort.sortImproved(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testSortImproved_EmptyArray() {
+        int[] arr = {};
+        int[] expected = {};
+        BubbleSort.sortImproved(arr);
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testSortImproved_LargeArray() {
+        int[] arr = {100, 54, 23, 89, 1, 65, 77, 33};
+        int[] expected = {1, 23, 33, 54, 65, 77, 89, 100};
+        BubbleSort.sortImproved(arr);
+        assertArrayEquals(expected, arr);
+    }
 }
